bc3d89c090e881aa85c7bab0e07103bf93cedcb9
vijay <vijaygohel5454@gmail.com>
2022-03-30 13:13:24 +0530

    created login page and oauth login using passport

--- app.js
+++ app.js
@@ -3,20 +3,40 @@ const colors = require('colors')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const connectDb = require('./config/connectDb')
+const passport = require("passport")
+const session  = require("express-session")
 
-dotenv.config({path: './config/config.env'});
+dotenv.config({path: './config/.env'});
 
 connectDb();
 
+require("./config/passport-config")(passport);
 
 const app = express();
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
 if(process.env.NODE_ENV === "development")
 {
     app.use(morgan('dev'));
 }
 
+app.use(express.static(__dirname+"/public"));
+
+app.set("view engine", "ejs" );
+
+app.use(session({
+    secret:process.env.SESSION_SECRET,
+    resave:false,
+    saveUninitialized:false
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use("/", require("./routes/index"));
+app.use("/auth", require("./routes/auth"));
 
 const PORT= process.env.PORT || 5000;
 
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,14 @@
-const express = require("express")
+const express = require("express");
 const router = express.Router();
 
-router.
-    route("/")
-    .get((req,res)=>{
-        res.send("login")
+router
+    .get("/",(req,res)=>{
+        res.render("login"); 
     });
 
-router.
-    route("/dashboard")
-    .get((req,res)=>{
-        res.send("dashboard")
+router
+    .get("/dashboard",(req,res)=>{
+        res.render("dashboard");
     });
 
 module.exports = router;
