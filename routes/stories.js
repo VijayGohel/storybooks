e2ba3f7fff26e1ed2a2b3551c7ee7a3920792387
vijay <vijaygohel5454@gmail.com>
2022-04-05 12:42:13 +0530

    add and store story

--- routes/stories.js
+++ routes/stories.js
@@ -8,4 +8,20 @@ router
         res.render("stories/add"); 
     });
 
+router
+    .post("/",ensureAuth,async (req,res)=>{
+
+        try {
+            req.body.user = req.user.id;
+
+            await Story.create(req.body);
+
+            res.redirect("/dashboard"); 
+        } catch (err) {
+            console.error(err);
+            res.render("Errors/500");
+        }
+        
+    });
+
 module.exports = router;
